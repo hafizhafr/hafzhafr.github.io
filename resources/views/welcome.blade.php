6da95d45b0a0ceecd8dff01f10117bd1ef486005
hafizhafr <hafizhabdullah520@gmail.com>
2023-04-10 04:19:12 +0700

    portofolio

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,243 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Portofolio | Hafizh Abdullah Fadlurohman</title>
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+	<link rel="icon" href="logo1.png" type="image/x-icon">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
+    <link rel="stylesheet" href="/libs/font-awesome/css/font-awesome.min.css">
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/styles.css" rel="stylesheet">
+</head>
+
+<body>
+    <div id="mobile-menu-open" class="shadow-large">
+        <i class="fa fa-bars" aria-hidden="true"></i>
+    </div>
+    <!-- End #mobile-menu-toggle -->
+    <header>
+        <div id="mobile-menu-close">
+            <span>Close</span> <i class="fa fa-times" aria-hidden="true"></i>
+        </div>
+        <ul id="menu" class="shadow">
+            <li>
+                <a href="#about">About Me</a>
+            </li>
+            <li>
+                <a href="#experience">Experience</a>
+            </li>
+            <li>
+                <a href="#education">Education</a>
+            </li>
+            <li>
+                <a href="#projects">Projects</a>
+            </li>
+            <li>
+                <a href="#skills">Skills</a>
+            </li>
+            <li>
+                <a href="#contact">Contact</a>
+            </li>
+        </ul>
+    </header>
+    <!-- End header -->
+
+    <div id="lead">
+        <div id="lead-content">
+            <h1>Hafizh AFR</h1>
+            <h2>41037006211029 TIF A1 /4 </h2>
+            <a href="https://drive.google.com/uc?export=download&id=15c_WTzAZ4hdvm8CbBllwtSPvJ4kxss9v" class="btn-rounded-white">Download Resume</a>
+        </div>
+        <!-- End #lead-content -->
+
+        <div id="lead-overlay"></div>
+
+        <div id="lead-down">
+            <span>
+                <i class="fa fa-chevron-down" aria-hidden="true"></i>
+            </span>
+        </div>
+        <!-- End #lead-down -->
+    </div>
+    <!-- End #lead -->
+
+    <div id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <h2 class="heading">About Me</h2>
+                </div>
+                <div class="col-md-8">
+                    <p>
+                    Fresh Graduate with experience as admin application i smart smkn 2 subang. An independent and purposeful individual with critical thinking and passion in product development. Passion to learn new things and contribute positively to a given project.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End #about -->
+
+    <div id="experience" class="background-alt">
+        <h2 class="heading">Experience</h2>
+        <div id="experience-timeline">
+            <div data-date="March 2021 – May 2021">
+                <h3>SMK Negeri 2 Subang</h3>
+                <h4>Admin</h4>
+                <ul>
+                    <li>Entering Teacher And Cadets Database From Microsoft Excel Into APK</li>
+                    <li>Assist Printing Reports In The Application</li>
+                    <li>Fix APP Bugs</li>
+                </ul>
+            </div>
+
+            <div data-date="September 2015 – September 2016">
+                <h3>PT Digijaya Mahakarya Teknologi</h3>
+                <h4>Full Stack Developer Internship</h4>
+                <ul>
+                    <li>Create database tables</li>
+                    <li>Working on the student data feature section in CRUD</li>
+                    <li>Create graphs and connect them according to the database of circles, bars, line graphs and display them on the homepage</li>
+                    <li>Create search and sort feature according to database</li>
+                    <li>Create excel, pdf data export and import features</li>
+                </ul>
+            </div>
+
+            <div data-date="September 2015 – September 2016">
+                <h3>Prakerin SMK Negeri 2 Subang</h3>
+                <h4>Full Stack Developer</h4>
+                <ul>
+                     <li>
+                     Making a Stempert financial Application
+                    </li>
+                    <li>
+                    Create database and connect
+                    </li>
+                    <li>
+                    Make interface display 3 users, admin, homeroom and cadets
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!-- End #experience -->
+
+    <div id="education">
+        <h2 class="heading">Education</h2>
+        <div class="education-block">
+            <h3>Universitas Islam Nusantara</h3>
+            <span class="education-date">Sept 2016 - Sept 2017</span>
+            <h4>Bachelor of Software Engineering</h4>
+           <ul>
+            <li>Deputy chairman of the informatics engineering student association</li>
+           </ul>
+        </div>
+        <!-- End .education-block -->
+
+        <div class="education-block">
+            <h3>SMK Negeri 2 Subang</h3>
+            <span class="education-date">Sept 2016 - Sept 2017</span>
+            <h4>Bachelor of Science in Computer Science</h4>
+            <ul>
+                <li>
+                Section Commander 10 Staff Batalyon cadets Smkn 2 Subang
+                </li>
+            </ul>
+        </div>
+        <!-- End .education-block -->
+    </div>
+    <!-- End #education -->
+
+    <div id="projects" class="background-alt">
+        <h2 class="heading">Projects</h2>
+        <div class="container">
+            <div class="row">
+                <div class="project shadow-large">
+                    <div class="project-image">
+                        <img src="images/i-smart2.png" />
+                    </div>
+                    <!-- End .project-image -->
+                    <div class="project-info">
+                        <h3>I Smart</h3>
+                        <p>
+                        Creating a Data Processing Application for the Report Management System of SMKN 2 Subang
+                        </p>
+                    </div>
+                    <!-- End .project-info -->
+                </div>
+                <!-- End .project -->
+
+                <!-- End .project -->
+            </div>
+        </div>
+    </div>
+    <!-- End #projects -->
+
+    <div id="skills">
+        <h2 class="heading">Skills</h2>
+        <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JS</li>
+            <li>PHP</li>
+            <li>MY SQL</li>
+            <li>C++</li>
+            <li>Codeigniter 3</li>
+            <li>Laravel 10</li>
+        </ul>
+    </div>
+    <!-- End #skills -->
+
+    <div id="contact">
+        <h2>Contact Us</h2>
+        <div id="contact-form">
+            <form method="POST" action="https://formspree.io/f/xgebpoqq">
+                <input type="hidden" name="_subject" value="Contact request from personal website" />
+                <input type="email" name="_replyto" placeholder="Your email" required>
+                <textarea name="message" placeholder="Your message" required></textarea>
+                <button type="submit">Send</button>
+            </form>
+        </div>
+        <!-- End #contact-form -->
+    </div>
+    <!-- End #contact -->
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-5 copyright">
+                    <p>
+                        Copyright &copy; <span id="current-year">2023</span> Hafizh Abdullah Fadlurohman
+                    </p>
+                </div>
+                <div class="col-sm-2 top">
+                    <span id="to-top">
+                        <i class="fa fa-chevron-up" aria-hidden="true"></i>
+                    </span>
+                </div>
+                <div class="col-sm-5 social">
+                    <ul>
+                        <li>
+                            <a href="https://github.com/hafizhafr" target="_blank"><i class="fa fa-github" aria-hidden="true"></i></a>
+                        </li>
+                        <li>
+                            <a href="https://www.linkedin.com/in/hafizh-abdullah-fadlurohman/" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                        </li>
+                        <li>
+                            <a href="https://www.facebook.com/" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- End footer -->
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="/js/scripts.min.js"></script>
+</body>
+
+</html>
